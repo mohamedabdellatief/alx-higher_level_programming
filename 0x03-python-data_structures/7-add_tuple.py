5cd3a301505cda214f0f3dcee1a22c6c3bdec66b
mohamed abdellatif <mohamedabdellatif@gmail.com>
2023-06-12 14:15:30 -0700

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+# 7-add_tuple.py
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a += (0, 0)
-    tuple_b += (0, 0)
-    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    """Add two tuples."""
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
 
-vi 8-multiple_returns.py
-
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0] if length > 0 else "None"
-    tup = length, first_char
-    return(tup)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
